fe2e7c9a30d1fe1b7206748320af353cae588ce4
llsgllsg <151439431+llsgllsg@users.noreply.github.com>
2026-01-17 22:38:37 +0800

    Update upload_to_ftp.py

--- upload_to_ftp.py
+++ upload_to_ftp.py
@@ -2,7 +2,7 @@ from ftplib import FTP
 import os
 
 # 从环境变量中获取 FTP 服务器信息
-ftp_server = 'cn-sy1.rains3.com'
+ftp_server = 'cn-nb1.rains3.com'
 ftp_port = 8021
 ftp_user = os.getenv('FTP_USERNAME')
 ftp_pass = os.getenv('FTP_PASSWORD')
@@ -13,7 +13,7 @@ ftp.connect(ftp_server, ftp_port)
 ftp.login(user=ftp_user, passwd=ftp_pass)
 
 # 切换到目标文件夹
-ftp.cwd('/main')
+ftp.cwd('/gf123')
 
 # 要上传的文件路径和名称
 filename = 'main.xaml'
